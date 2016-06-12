8af5c136c94081b1f506a088900c3d2e2a40262a
Kartikaya Gupta <kgupta@mozilla.com>
2016-06-11 23:06:49 -0400

    Update to recursively print subparts

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,21 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
+fn dump(pfx: &str, pm: &mailparse::ParsedMail) {
+    println!(">> Headers from {} <<", pfx);
+    for h in &pm.headers {
+        println!("  [{}] => [{}]", h.get_key().unwrap(), h.get_value().unwrap());
+    }
+    println!(">> Body from {} <<", pfx);
+    println!("  [{}]", pm.get_body().unwrap());
+    let mut c = 1;
+    for s in &pm.subparts {
+        println!(">> Subpart {} <<", c);
+        dump("subpart", s);
+        c = c + 1;
+    }
+}
+
 fn main() {
     let mut args = env::args();
     args.next();
@@ -11,14 +26,11 @@ fn main() {
         match args.next() {
             None => break,
             Some(a) => {
-                let mut f = File::open(a.clone()).unwrap();
+                let mut f = File::open(&a).unwrap();
                 let mut d = Vec::<u8>::new();
                 f.read_to_end(&mut d).unwrap();
-                let (headers, _) = mailparse::parse_headers(&d).unwrap();
-                println!(">> Headers from {} <<", a);
-                for h in headers {
-                    println!("  [{}] => [{}]", h.get_key().unwrap(), h.get_value().unwrap());
-                }
+                let mail = mailparse::parse_mail(&d).unwrap();
+                dump(&a, &mail);
             }
         }
     }

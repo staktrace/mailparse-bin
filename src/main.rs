3df551b6685fb575ce4851390b9836c219f86878
Kartikaya Gupta <kats@trevize.staktrace.com>
2019-11-09 05:03:34 -0500

    Update to latest mailparse and include addrparse output

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,18 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
+use mailparse::MailHeaderMap;
+
 fn dump(pfx: &str, pm: &mailparse::ParsedMail) {
     println!(">> Headers from {} <<", pfx);
     for h in &pm.headers {
         println!("  [{}] => [{}]", h.get_key().unwrap(), h.get_value().unwrap());
     }
+    println!(">> Addresses from {} <<", pfx);
+    pm.headers.get_first_value("From").unwrap().map(|a| println!("{:?}", mailparse::addrparse(&a).unwrap()));
+    pm.headers.get_first_value("To").unwrap().map(|a| println!("{:?}", mailparse::addrparse(&a).unwrap()));
+    pm.headers.get_first_value("Cc").unwrap().map(|a| println!("{:?}", mailparse::addrparse(&a).unwrap()));
+    pm.headers.get_first_value("Bcc").unwrap().map(|a| println!("{:?}", mailparse::addrparse(&a).unwrap()));
     println!(">> Body from {} <<", pfx);
     if pm.ctype.mimetype.starts_with("text/") {
         println!("  [{}]", pm.get_body().unwrap());
